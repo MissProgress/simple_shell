cc492a26e028756ce606a950a75a3ca56070ec7a
Mwakai <mwakaimwambala@gmail.com>
2023-12-05 14:01:47 -0800

    myshell 1

---
+++ myshell.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define MAX_INPUT_SIZE 1024
+
+void display_prompt() {
+    printf("#cisfun$ ");
+    fflush(stdout);
+}
+
+int main() {
+    char input[MAX_INPUT_SIZE];
+
+    while (1) {
+        display_prompt();
+
+        // Read a line from the user
+        if (fgets(input, sizeof(input), stdin) == NULL) {
+            if (feof(stdin)) {
+                // Handle end of file (Ctrl+D)
+                printf("\n");
+                break;
+            }
+            perror("Error reading input");
+            exit(EXIT_FAILURE);
+        }
+
+        // Remove newline character
+        input[strcspn(input, "\n")] = '\0';
+
+        // Fork a child process
+        pid_t pid = fork();
+
+        if (pid == -1) {
+            perror("Fork failed");
+            exit(EXIT_FAILURE);
+        } else if (pid == 0) {
+            // Child process
+            execlp(input, input, NULL);
+
+            // If exec fails
+            perror("Command not found");
+            exit(EXIT_FAILURE);
+        } else {
+            // Parent process
+            int status;
+            waitpid(pid, &status, 0);
+
+            if (WIFEXITED(status)) {
+                // Child process exited normally
+                printf("process exited with status %d\n", WEXITSTATUS(status));
+            } else if (WIFSIGNALED(status)) {
+                // Child process terminated by a signal
+                printf("Process terminated by signal %d\n", WTERMSIG(status));
+            }
+        }
+    }
+
+    printf("Exiting shell.\n");
+    return 0;
+}
+
