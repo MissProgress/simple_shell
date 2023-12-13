a7c21871d7e94e953083a87cdd994474729f60af
Mwakai <mwakaimwambala@gmail.com>
2023-12-12 16:01:19 -0800

    betty.c

---
+++ betty.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - The main function of the program.
+ *
+ * Return: 0 on success, non-zero on failure.
+ */
+int main(void)
+{
+    printf("Betty would be proud!\n");
+
+    return 0;
+}
+
